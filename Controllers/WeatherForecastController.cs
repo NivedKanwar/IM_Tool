09a29b3c789b380d1102059b08f3129e4a65154c
Nived Kanwar <creedrace1@gmail.com>
2023-05-04 02:21:23 +0530

    Algorithms added

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace IM_Tool.Controllers
 {
@@ -35,5 +37,125 @@ namespace IM_Tool.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet]
+        [Route("/ExistingUserLogin/{username}/{password}")]
+        public IActionResult GetExistingUserLogin(string username,string password)
+        {
+        var UE = new UnicodeEncoding();
+        byte[] passwordBytes = UE.GetBytes(username);
+        byte[] aesKey = SHA256.HashData(passwordBytes);
+        byte[] aesIV =MD5.HashData(passwordBytes);
+        var secretKey = Encoding.UTF8.GetString(aesKey);
+        var encryptedUser = EncryptUsingAES(username,aesKey,aesIV);
+        var cookieOptions = new CookieOptions{Expires = DateTime.Now.AddDays(30)};
+        HttpContext.Response.Cookies.Append("Legacy_User", Base64Encode(username),cookieOptions);
+        HttpContext.Response.Cookies.Append("Secret_Key", secretKey , cookieOptions);
+        HttpContext.Response.Cookies.Append("New_User", encryptedUser,cookieOptions);
+        return Ok();
+        }
+
+private static byte[] EncryptUsingAES(string plainText, byte[] Key, byte[] IV) 
+{
+        byte[] encrypted;
+        using(AesManaged aes = new AesManaged()) {
+            ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
+            using(MemoryStream ms = new MemoryStream()) {
+                using(CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write)) {
+                    using(StreamWriter sw = new StreamWriter(cs))
+                    sw.Write(plainText);
+                    encrypted = ms.ToArray();
+                }
+            }
+        }
+        return encrypted;
+}
+
+    private static string DecryptUsingAES(byte[] cipherText, byte[] Key, byte[] IV) 
+    {
+        string plaintext = null;
+        using(AesManaged aes = new AesManaged()) {
+            ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+            using(MemoryStream ms = new MemoryStream(cipherText)) {
+                using(CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read)) {
+                    using(StreamReader reader = new StreamReader(cs))
+                    plaintext = reader.ReadToEnd();
+                }
+            }
+        }
+        return plaintext;
+    }
+        private static byte[] EncryptUsingTripleDES(string plainText, byte[] Key, byte[] IV) 
+        {  
+        byte[] encrypted;  
+        using(TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider()) 
+        {  
+            ICryptoTransform encryptor = tdes.CreateEncryptor(Key, IV);  
+            using(MemoryStream ms = new MemoryStream()) {  
+                using(CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write)) {  
+                    using(StreamWriter sw = new StreamWriter(cs))  
+                    sw.Write(plainText);  
+                    encrypted = ms.ToArray();  
+                }  
+            }  
+        }  
+        return encrypted;  
+        }  
+
+    private static string DecryptUsingTripleDES(byte[] cipherText, byte[] Key, byte[] IV)
+     {  
+        string plaintext = null;  
+        using(TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider()) {  
+            ICryptoTransform decryptor = tdes.CreateDecryptor(Key, IV);  
+            using(MemoryStream ms = new MemoryStream(cipherText)) {  
+                using(CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read)) {  
+                    using(StreamReader reader = new StreamReader(cs))  
+                    plaintext = reader.ReadToEnd();  
+                }  
+            }  
+        }  
+        return plaintext;  
+     }
+
+     private static public byte[] EncryptionUsingRES(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)  
+{  
+ try  
+ {  
+ byte[] encryptedData;  
+ using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())  
+   {  
+    RSA.ImportParameters(RSAKey);  
+           encryptedData = RSA.Encrypt(Data, DoOAEPPadding);  
+   }   return encryptedData;  
+ }  
+ catch (CryptographicException e)  
+ {  
+ Console.WriteLine(e.Message);  
+ return null;  
+ }  
+} 
+
+private static public byte[] DecryptionUsingRES(byte[]Data, RSAParameters RSAKey, bool DoOAEPPadding)  
+{  
+ try  
+ {  
+ byte[] decryptedData;  
+ using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())  
+    {  
+     RSA.ImportParameters(RSAKey);  
+     decryptedData = RSA.Decrypt(Data, DoOAEPPadding);  
+    }  
+ return decryptedData;  
+ }  
+ catch (CryptographicException e)  
+ {  
+ Console.WriteLine(e.ToString());  
+ return null;  
+ }          
+}
+private static string Base64Encode(string plainText){
+    var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+    return System.Convert.ToBase64String(plainTextBytes);
+}
     }
 }
